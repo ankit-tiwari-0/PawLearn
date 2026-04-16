4f91b60c86e19a202f6965318fe92749d9668361
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-04-16 20:51:21 +0530

    add gsap in herosec

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,11 +1,98 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import 'remixicon/fonts/remixicon.css'
 
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(useGSAP, ScrollTrigger)
+
 const HeroSection = () => {
+    let leftRef = useRef()
+    let rightRef = useRef()
+    let sectRef = useRef()
+     
+    useGSAP(()=>{
+    
+     const tl = gsap.timeline({
+         scrollTrigger: {
+      trigger: sectRef .current,
+      start: "top 80%",
+      toggleActions: "play reverse play reverse"
+    }
+     })
+      
+    tl.from(leftRef.current.querySelectorAll('h3'), {
+    x: 50,
+    opacity: 0,
+    duration: 0.5
+  })
+  .from(leftRef.current.querySelectorAll('h1'), {
+    y: 30,
+    opacity: 0,
+    duration: 0.6
+  })
+  .from(leftRef.current.querySelectorAll('p'), {
+    x: 60,
+    opacity: 0,
+    duration: 0.6
+  })
+  .from(leftRef.current.querySelectorAll('button'), {
+    scale: 0.8,
+    opacity: 0,
+    duration: 0.5
+  })
+  .to(leftRef.current.querySelectorAll('button'), {
+  backgroundColor: "#fb933c",   // your orange
+  color: "white",             // white text
+  duration: 0.5
+})
+
+  // ICONS
+  .from(leftRef.current.querySelectorAll('.icon > div'), {
+    opacity: 0,
+    y: 50,
+    stagger: 0.15,
+    duration: 0.4
+  })
+  .from(rightRef.current.querySelectorAll('.topleft'), {
+    x: -80,
+    opacity: 0,
+    scale: 0.85,
+    duration: 0.8,
+    stagger: 0.25,
+    ease: "power3.out"
+  })
+  .from(rightRef.current.querySelectorAll('.topright'), {
+    x: 80,
+    opacity: 0,
+    scale: 0.85,
+    duration: 0.8,
+    stagger: 0.25,
+    ease: "power3.out"
+  })
+  .from(rightRef.current.querySelectorAll('.bottomleft'), {
+    y: 80,
+    opacity: 0,
+    scale: 0.85,
+    duration: 0.8,
+    stagger: 0.25,
+    ease: "power3.out"
+  })
+  .from(rightRef.current.querySelectorAll('.bottomright'), {
+    y: 80,
+    opacity: 0,
+    scale: 0.85,
+    duration: 0.8,
+    stagger: 0.25,
+    ease: "power3.out"
+  })
+    },{ scope: sectRef  })
+
     return (
         <div className='w-full  bg-white border-2 border-transparent rounded-xl py-7 px-4'>
-            <div className='lg:flex'>
-                <div className='px-3 lg:px-10 lg:bg-white bg-gray-100 rounded-xl lg:pb-0.5 pb-8 '>
+            <div ref={sectRef} className='lg:flex'>
+                <div ref={leftRef} className='px-3 lg:px-10 lg:bg-white bg-gray-100 rounded-xl lg:pb-0.5 pb-8 '>
                     <h3 className='text-orange-400 lg:text-2xl text-xl py-1.5 font-bold'>SErvices</h3>
                     <h1 className='text-xl lg:text-5xl font-bold mb-5 ml-5 '>Train Your Dog
                         <br />
@@ -13,10 +100,10 @@ const HeroSection = () => {
                     <p className='max-w-xs lg:max-w-md ml-5 mb-10'>Train your dog effectively from home
                         with expert-guided lessons, real-time feedback, and simple step-by-step
                         techniques designed for everyday pet owners</p>
-                    <button className='bg-orange-400 text-white text-2xl px-3 py-3 rounded-2xl ml-5 '> Start Yee Free Trail</button>
+                    <button className='bg-orange-100 text-black text-2xl px-3 py-3 rounded-2xl ml-5 '> Start Yee Free Trail</button>
 
 
-                    <div className="flex gap-4  mt-10 ml-10 ">
+                    <div  className="icon flex gap-4  mt-10 ml-10 ">
 
                         {[
                             "ri-mental-health-line",
@@ -31,11 +118,11 @@ const HeroSection = () => {
 
                     </div>
                 </div>
-                <div className="grid grid-cols-2 gap-6 lg:ml-55 lg:py-5 py-10  w-full justify-center">
-                    <div className="w-2/2 lg:w-72 h-40 lg:h-60 rounded-2xl  bg-[url('/img/b.avif')] bg-cover bg-center flex items-end px-8 lg:px-12 py-2 ">
+                <div ref={rightRef} className="grid grid-cols-2 gap-6 lg:ml-55 lg:py-5 py-10  w-full justify-center">
+                    <div className="topleft w-2/2 lg:w-72 h-40 lg:h-60 rounded-2xl  bg-[url('/img/b.avif')] bg-cover bg-center flex items-end px-8 lg:px-12 py-2 ">
                         <button className='lg:px-10 px-3 lg:py-5 py-2 rounded-2xl bg-white/20 backdrop-blur-md text-white'>Instructor</button>
                     </div>
-                    <div className="lg:w-72 h-44 lg:h-60 bg-orange-400 rounded-2xl px-4 py-4 flex flex-col justify-between">
+                    <div className="topright lg:w-72 h-44 lg:h-60 bg-orange-400 rounded-2xl px-4 py-4 flex flex-col justify-between">
 
                         <div>
                             <h1 className="mb-2 text-white font-bold text-lg  ">
@@ -48,7 +135,7 @@ const HeroSection = () => {
                             </p>
                         </div>
 
-                        <div className="flex  items-center gap-3">
+                        <div  className="flex  items-center gap-3">
                             <button className="px-3 py-2 bg-white/20 backdrop-blur-md text-white text-sm rounded-full">
                                 Smart
                             </button>
@@ -60,7 +147,7 @@ const HeroSection = () => {
 
                     </div>
 
-                    <div className="lg:w-72 h-44 lg:h-60 border-2 border-gray-200 rounded-2xl px-4 py-4 flex flex-col justify-between">
+                    <div className=" bottomleft lg:w-72 h-44 lg:h-60 border-2 border-gray-200 rounded-2xl px-4 py-4 flex flex-col justify-between">
 
                         <div>
                             <h1 className="mb-2 text-black font-bold text-lg  ">
@@ -85,7 +172,7 @@ const HeroSection = () => {
 
                     </div>
                     
-                    <div className=" lg:w-72 h-40 rounded-2xl lg:h-60 bg-[url('/img/t.avif')] bg-cover bg-center flex items-end px-8 lg:px-12 py-2">
+                    <div className=" bottomright lg:w-72 h-40 rounded-2xl lg:h-60 bg-[url('/img/t.avif')] bg-cover bg-center flex items-end px-8 lg:px-12 py-2">
 
                         <button className='lg:px-10 px-3 lg:py-5 py-2 rounded-2xl bg-white/10 backdrop-blur-md text-white'>See Progress</button>
 
